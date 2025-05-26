d849f843956661d2c534797bce599546afb63d87
Nokskiy <tsflafsu@yandex.ru>
2025-05-26 12:25:46 +0500

    Music name read

--- src/AvalonixAPI/MediaPlayer.cs
+++ src/AvalonixAPI/MediaPlayer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,15 +13,18 @@ namespace AvalonixAPI
     {
         private static WaveOutEvent _playingMusic = null!;
 
-        private static float _totalMusicTime;
+        private static float _totalMusicTime = 0;
+        private static string _musicName = null!;
         public static void Play(string path)
         {
             using (var audioFile = new AudioFileReader(path))
             {
+                _musicName = Path.GetFileName(path);
                 _totalMusicTime = (float)audioFile.TotalTime.TotalSeconds;
                 _playingMusic = new WaveOutEvent();
                 _playingMusic.Init(audioFile);
                 _playingMusic.Play();
+
                 while (_playingMusic != null && Playing())
                 {
                     Thread.Sleep(1000);
@@ -56,6 +60,11 @@ namespace AvalonixAPI
         public static float TotalMusicTime()
         {
             return _totalMusicTime;
+        }
+
+        public static string MusicName()
+        {
+            return _musicName;
         }
     }
 }
