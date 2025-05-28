a8c5303d1f0c4da55c3a8a2a0e9d4198c509c279
ruzen42 <malledefer@gmail.com>
2025-05-28 14:32:10 +0500

    fix

--- src/Program.cs
+++ src/Program.cs
@@ -1,23 +1,21 @@
 ﻿using Avalonia;
+using System;
 using NeoSimpleLogger;
-using static NeoSimpleLogger.Logger;
 
 namespace Avalonix;
 
 public class Program
 {
-    public static Logger? Logger { get; private set; }
+    public static Logger Logger = new(Logger.TypeLogger.Console);
     [STAThread]
     public static void Main(string[] args)
     {
-        Logger = new Logger(TypeLogger.Console);
         Logger.Info("App started");
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
 
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
+    private static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
