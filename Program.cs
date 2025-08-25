854c15d89407478b7581b7d72e43ab6242378c4b
ruzen42 <malledeferlive@gmail.com>
2025-08-25 16:07:45 +0500

    fix main

--- Program.cs
+++ Program.cs
@@ -9,38 +9,10 @@ namespace Avalonix;
 internal static class Program
 {
     [STAThread]
-    public static void Main(string[] args)
-    {
-        
-        var playlist = DiskManager.GetPlaylist("test");
-        /*
-        //playlist.AddTrack(new Track("F:\\Плейлисты\\Accept\\Accept - Fast As A Shark.mp3"));
-        
-        playlist.PlaylistData.Tracks[0].IncreaseRarity(1);
-        playlist.PlaylistData.Tracks[0].UpdateLastListenDate();
-        playlist.UpdateLastListenDate();
-        
-        
-        playlist.Save();
-        
-        //Console.WriteLine(playlist.Tracks[0].TrackData.LastListen.Date);
-        
-        //DiskManager.SavePlaylist(playlist);
-        */
-        
-        var player = new MediaPlayer(new Logger());
-        player.Play(playlist.PlaylistData.Tracks[0]);
-        player.Pause();
-        player.Resume();
-        player.Stop();
-        
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
-    }
-
-    private static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    public static void Main(string[] args) =>
+        AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .StartWithClassicDesktopLifetime(args);
 }
