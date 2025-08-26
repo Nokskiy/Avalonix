c302963d8cd5f894c5028e808569eba37f535f14
ruzen42 <malledeferlive@gmail.com>
2025-08-26 14:35:24 +0500

    switch to multifile arch

--- Models/Disk/DiskManager.cs
+++ Models/Disk/DiskManager.cs
@@ -71,19 +71,6 @@ public class DiskManager : IDiskWriter, IDiskLoader
     }
 }
 
-public interface IDiskWriter
-{
-    public void Write<T>(T obj, string path)
-    {
-        var opt = new JsonSerializerOptions
-        {
-            WriteIndented = true,
-            IncludeFields = true,
-            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-        };
-
-        if (!File.Exists(path))
-            File.Create(path).Close();
 
         File.WriteAllText(path, JsonSerializer.Serialize(obj, opt));
     }
