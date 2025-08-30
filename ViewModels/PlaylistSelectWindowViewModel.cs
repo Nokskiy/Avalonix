e27d97a8e550ee2cd5040f54b7a2efc6f3025eed
ruzen42 <malledeferlive@gmail.com>
2025-08-30 15:18:33 +0500

    fix

--- ViewModels/PlaylistSelectWindowViewModel.cs
+++ ViewModels/PlaylistSelectWindowViewModel.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Avalonix.Models.Media.PlaylistFiles;
+
 namespace Avalonix.ViewModels;
 
-public class PlaylistSelectWindowViewModel : ViewModelBase;
+public class PlaylistSelectWindowViewModel : ViewModelBase
+{
+    public List<Playlist> GetPlaylists() =>
+        throw new NotImplementedException();
+}
--- Views/SecondaryWindows/PlaylistSelectWindow/PlaylistSelectWindow.axaml.cs
+++ Views/SecondaryWindows/PlaylistSelectWindow/PlaylistSelectWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonix.Models.Media.PlaylistFiles;
 using Avalonix.ViewModels;
 using Microsoft.Extensions.Logging;
 
@@ -6,9 +9,14 @@ namespace Avalonix.Views.SecondaryWindows.PlaylistSelectWindow;
 
 public partial class PlaylistSelectWindow : Window
 {
+    private List<Playlist> _playlists;
     public PlaylistSelectWindow(ILogger logger, PlaylistSelectWindowViewModel vm)
     {
         InitializeComponent();
         logger.LogInformation("PlaylistCreateWindow opened");
+    }
+
+    private void StartPlaylist_OnClick(object? sender, RoutedEventArgs e)
+    {
     }
 }
