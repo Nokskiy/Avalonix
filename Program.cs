aa95833097f5702a9bb53fcf0f77e13ac9560137
Nokskiy <tsfkafsu@yandex.ru>
2025-09-03 20:50:11 +0500

    Clean Main

--- Program.cs
+++ Program.cs
@@ -13,17 +13,8 @@ namespace Avalonix;
 internal static class Program
 {
     [STAThread]
-    public static async Task Main(string[] args)
-    {
-        Logger logger = new();
-        var playlist = await new DiskManager(logger, new MediaPlayer(logger)).GetPlaylist("test");
-        //await playlist?.AddTrack(new Track("F:\\Плейлисты\\Metallica\\Metallica - Enter Sandman (Remastered 2021).mp3"))!;
-
-        SortPlaylistTrackFlags flags = SortPlaylistTrackFlags.ArtistInverted;
-        
-        await playlist?.SortTracks(flags)!;
+    public static void Main(string[] args) =>
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-    }
 
     private static AppBuilder BuildAvaloniaApp() =>
         AppBuilder.Configure<App>().LogToTrace().UsePlatformDetect();
