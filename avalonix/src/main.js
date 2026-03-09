244d608d5ab0d2a6ae1a5723ddbb98bb6ce7320e
Nokskiy <145178078+Nokskiy@users.noreply.github.com>
2026-03-09 20:50:29 +0500

    change(new calculation for gradient)

--- avalonix/src/main.js
+++ avalonix/src/main.js
@@ -18,8 +18,8 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 setInterval(() => {
-  let x = window.screenX /2;
-  let y = window.screenY /2;
+  let x = window.screenX /2 + window.innerWidth / 2;
+  let y = window.screenY /2  + window.innerHeight /2;
   const col1 = getComputedStyle(document.documentElement).getPropertyValue("--background-gradient1");
   const col2 = getComputedStyle(document.documentElement).getPropertyValue("--background-gradient2");
 	document.body.style.background = `radial-gradient(circle 350px at ${x}px ${y}px, ${col1}, ${col2} )`;
