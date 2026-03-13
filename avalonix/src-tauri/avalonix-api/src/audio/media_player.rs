1ab606344dd77ff3f1277a035044530be6ff4480
Nokskiy <145178078+Nokskiy@users.noreply.github.com>
2026-03-13 14:31:15 +0500

    cleanup

--- avalonix/src-tauri/avalonix-api/src/audio/media_player.rs
+++ avalonix/src-tauri/avalonix-api/src/audio/media_player.rs
@@ -18,7 +18,6 @@ struct Playback {
     last_device_description: DeviceDescription,
     player: Player,
     last_playing_track_path: Option<String>,
-    last_playing_time: Option<Duration>,
 }
 
 pub struct MediaPlayer {
@@ -30,17 +29,10 @@ impl<'a> Playback {
         let sink_handle =
             rodio::DeviceSinkBuilder::open_default_sink().expect("open default audio stream");
 
-        let file = BufReader::new(
-            File::open("D:\\music\\Three Days Grace [restored]\\2006 - One-X\\03. Animal I Have Become.flac").unwrap(),
-        );
-
-        let source = Decoder::new(file).unwrap();
-
         let mixer = sink_handle.mixer();
         let player = Player::connect_new(mixer);
-        player.append(source);
 
-        player.get_pos(); // for get last pos
+        player.get_pos();
 
         let host = Host::default();
 
@@ -49,7 +41,6 @@ impl<'a> Playback {
             last_device_description: host.default_output_device().unwrap().description().unwrap(),
             player,
             last_playing_track_path: None,
-            last_playing_time: None,
         }
     }
 
@@ -57,17 +48,30 @@ impl<'a> Playback {
         let sink_handle =
             rodio::DeviceSinkBuilder::open_default_sink().expect("open default audio stream");
 
-        // Load a sound from a file, using a path relative to Cargo.toml
-        let file = BufReader::new(
-            File::open("D:\\music\\Three Days Grace [restored]\\2006 - One-X\\03. Animal I Have Become.flac").unwrap(),
-        );
-        // Note that the playback stops when the player is dropped
-        let player = rodio::play(&sink_handle.mixer(), file).unwrap();
+        let file_path = self.last_playing_track_path.as_ref().unwrap();
+        println!("{}", file_path);
+        let file = BufReader::new(File::open(file_path).unwrap());
+
+        self.player.stop();
+
+        let player = Player::connect_new(&sink_handle.mixer());
 
         let host = Host::default();
+
+        let file = BufReader::new(File::open(&file_path).unwrap());
+        let source = Decoder::new(file).unwrap();
+
+        player.append(source);
+
         self.mixer = sink_handle;
         self.last_device_description = host.default_output_device().unwrap().description().unwrap();
         self.player = player;
+    }
+
+    fn play(&mut self, file_path: String) {
+        let file = BufReader::new(File::open(file_path).unwrap());
+        let source = Decoder::new(file).unwrap();
+        self.player.append(source);
     }
 }
 
@@ -78,45 +82,57 @@ impl MediaPlayer {
         }
     }
 
-    pub fn play(&mut self) {
-        let playback = self.playback.as_ref().unwrap();
-        let player = &playback.player;
+    pub fn play(&mut self, file_path: String) {
+        let playback = self.playback.as_mut().unwrap();
 
-        let source = SineWave::new(440.0)
-            .take_duration(Duration::from_secs_f32(0.25))
-            .amplify(0.20)
-            .repeat_infinite();
-        player.append(source);
+        playback.last_playing_track_path = Some(file_path.clone());
+        playback.play(file_path);
     }
 
     pub fn stop(&mut self) {
         self.playback.as_ref().unwrap().player.stop();
     }
 
-    pub fn update() {}
+    pub fn get_pos(&mut self) -> Duration {
+        self.playback.as_ref().unwrap().player.get_pos()
+    }
+
+    pub fn update(clone: Arc<Mutex<MediaPlayer>>) {
+        thread::spawn(move || {
+            let host = Host::default();
+            loop {
+                let device = host.default_output_device().unwrap();
+                thread::sleep(Duration::new(1, 0));
+                {
+                    let mut guard = clone.try_lock().unwrap();
+
+                    if guard.playback.as_ref().unwrap().last_device_description
+                        != device.description().unwrap()
+                    {
+                        let playback: &mut Playback = guard.playback.as_mut().unwrap();
+                        playback.change_device();
+                    }
+                }
+            }
+        });
+    }
 }
 
 #[test]
 fn test_play() {
     let player = Arc::new(Mutex::new(MediaPlayer::new()));
-    let clone = player.clone();
 
-    thread::spawn(move || {
-        let host = Host::default();
-        loop {
-            let device = host.default_output_device().unwrap();
-            thread::sleep(Duration::new(1, 0));
-            {
-                let mut guard = clone.try_lock().unwrap();
-                if guard.playback.as_ref().unwrap().last_device_description
-                    != device.description().unwrap()
-                {
-                    let a = guard.playback.as_mut().unwrap();
-                    a.change_device();
-                }
-            }
-        }
-    });
+    let clone1 = player.clone();
+    let clone2 = player.clone();
+
+    let file_path =
+        "D:\\music\\Three Days Grace [restored]\\2006 - One-X\\03. Animal I Have Become.flac"
+            .to_string();
+
+    MediaPlayer::update(clone2);
+
+    clone1.lock().as_mut().unwrap().play(file_path);
+
     loop {}
 }
 // it took too much nerves
