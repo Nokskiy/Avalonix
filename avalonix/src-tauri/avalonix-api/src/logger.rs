58a90633a13709567a1fe26fad88c029ad8a1b18
ruzen42 <malledeferlive@gmail.com>
2026-03-15 12:22:10 +0500

    fix(settings path)

--- avalonix/src-tauri/avalonix-api/src/logger.rs
+++ avalonix/src-tauri/avalonix-api/src/logger.rs
@@ -1,23 +1,23 @@
 use colored::Colorize;
 
 pub fn info(msg: &str) {
-    println!("{}", "INFO ".green().to_string() + msg);
+    println!("{} {}", "INFO".green(), msg);
 }
 
 pub fn error(msg: &str) {
-    println!("{}", "ERROR ".red().to_string() + msg);
+    println!("{} {}", "ERROR".red(), msg);
 }
 
 pub fn debug(msg: &str) {
-    println!("{}", "DEBUG ".blue().to_string() + msg);
+    println!("{} {}", "DEBUG".blue(), msg);
 }
 
 pub fn fatal(msg: &str) {
-    println!("{}", "FATAL ".red().to_string() + msg);
+    println!("{} {}", "FATAL".red().bold(), msg);
 }
 
 pub fn warn(msg: &str) {
-    println!("{}", "WARN ".yellow().to_string() + msg);
+    println!("{} {}", "WARN".yellow(), msg);
 }
 
 #[test]
